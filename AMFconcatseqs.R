d8c97a32055db526caf4fc94afbd6f2a916f5736
c383d893 <66577803+c383d893@users.noreply.github.com>
2021-10-05 14:39:20 +0200

    Add files via upload

---
+++ AMFconcatseqs.R
@@ -0,0 +1,25 @@
+####### Load required packages #######
+
+library(Biostrings)
+library(stringr)
+
+#read in fasta to get names
+SeqstoAlign <- readDNAStringSet('BLAST_otu97plusV15_3.23.21_cut.fasta')
+
+#extract forward and reverse reads, then join
+names<-names(SeqstoAlign)
+
+# bring alignments here and concatenate:
+
+#read in database and format seqs based on study cutoffs
+forward.aligned <- readDNAStringSet('R1.BLAST_otu97plusV15_3.23.21_cut_out/aligned-dna-sequences.fasta')
+reverse.aligned <- readDNAStringSet('R2.BLAST_otu97plusV15_3.23.21_cut_out/aligned-dna-sequences.fasta')
+
+#extract forward and reverse reads, then join
+joined.aligned<-paste(forward.aligned,reverse.aligned, sep="NNNNNNNNNN") # don't need Ns anymore, but doesn't hurt
+
+joined.aligned<-DNAStringSet(joined.aligned) #turn into DNAString
+names(joined.aligned)<-names #add names back in
+str(joined.aligned) #check looks ok
+
+writeXStringSet(joined.aligned, "aligned_BLAST_otu97plusV15_3.23.21_cut.fasta",width=10000) #write out.
---
+++ AMFtrimmedToOTUs.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=64gb
+#SBATCH --partition=sixhour
+#SBATCH --output=./slurmOutputs/AMFtrimmedToOTUs.out
+#SBATCH --error=./slurmOutputs/AMFtrimmedToOTUs.out
+#SBATCH --job-name=trm2otu
+
+module purge  
+module load R
+module load qiime2
+module load blast+
+module list
+
+# Replace placeholder text in R script with user-provided truncation lengths for R1 and R2: DADA2
+cat AMFdada2.R | sed "s/R1trunclen.value/$R1cutoff/" | sed "s/R2trunclen.value/$R2cutoff/" > AMFdada2withCutoffs.R
+
+# Run DADA2 pipeline
+echo;echo "Beginning DADA2 pipeline..."
+Rscript AMFdada2withCutoffs.R
+echo;echo "DADA2 pipeline complete. Converting output files to Qiime format..."
+rm AMFdada2withCutoffs.R # remove temporary script once it's done running
+
+# Convert ASV table from .tsv format to .biom format
+biom convert -i ./dada2output/ASVtable.tsv -o ./q2files/ASVtable.biom --to-hdf5 --table-type="OTU table"
+
+# Convert ASV table from .biom to .qza, ASV sequences from .fasta to .qza
+qiime tools import --input-path ./q2files/ASVtable.biom --type 'FeatureTable[Frequency]' --input-format BIOMV210Format --output-path ./q2files/ASVtable.qza
+qiime tools import --input-path ./dada2output/ASVs.fasta --type 'FeatureData[Sequence]' --output-path ./q2files/ASVseqs.qza
+
+# Convert reference database from .fasta to .qza
+qiime tools import --input-path ./V15_LSUDB_3.23.21.fasta --type 'FeatureData[Sequence]' --output-path ./q2files/AMFreferenceSeqs.qza
+echo; echo "ASV table, ASV sequences, and reference sequences have been converted to .qza"
+
+# OTU clustering
+qiime vsearch cluster-features-open-reference --i-table ./q2files/ASVtable.qza --i-sequences ./q2files/ASVseqs.qza --i-reference-sequences ./q2files/AMFreferenceSeqs.qza --p-perc-identity 0.97 --o-clustered-table ./q2files/otu97table.qza --o-clustered-sequences ./q2files/otu97repseqs.qza --o-new-reference-sequences ./q2files/otu97newRefSeqs.qza --verbose
+echo; echo "ASVs have been clustered into OTUs at 97% sequence similarity"
+
+# Additional chimera removal
+echo;echo "Beginning Additional chimera removal step..."
+qiime vsearch uchime-denovo --i-table ./q2files/otu97table.qza --i-sequences ./q2files/otu97repseqs.qza --output-dir uchime-dn-out
+qiime feature-table filter-features --i-table ./q2files/otu97table.qza --m-metadata-file uchime-dn-out/nonchimeras.qza --o-filtered-table ./q2files/otu97table.chim.qza
+echo; echo "Additional uchime chimera removal step complete"
+
+# OTU table cleanup 
+# Remove OTUs that occur fewer than 5 times
+qiime feature-table filter-features --i-table ./q2files/otu97table.chim.qza --p-min-frequency 5 --o-filtered-table ./q2files/otu97table_minusRareOTUs_allSamples.qza --verbose
+# Remove samples with fewer than 1000 reads
+qiime feature-table filter-samples --i-table ./q2files/otu97table_minusRareOTUs_allSamples.qza --p-min-frequency 1000 --o-filtered-table ./q2files/otu97table_clean.qza --verbose
+# Update OTU representative sequences to exclude OTUs that were removed:
+qiime feature-table filter-seqs --i-data ./q2files/otu97repseqs.qza --i-table ./q2files/otu97table_clean.qza --o-filtered-data ./q2files/otu97repseqs_clean.qza --verbose
+echo; echo "Rare OTUs and low-coverage samples have been removed from the dataset."
+
+# Export OTU representative sequences into .fasta format:
+# Qiime2 forces output into a file called dna-sequences.fasta . We direct it into ./q2files/
+qiime tools export --input-path ./q2files/otu97repseqs_clean.qza --output-path ./q2files/
+cat ./q2files/dna-sequences.fasta | sed 's/>/>OTU/g'|sed 's/ASV//g' > ./otu97repseqs_clean.fasta # rename file, relabel ASVs as OTUs, and any perfect matched to database append OTU and move to working directory.
+
+# Export OTU table into .tsv format:
+# Qiime2 forces output into a .biom file called feature-table.biom . We direct it into ./q2files/
+qiime tools export --input-path ./q2files/otu97table_clean.qza --output-path ./q2files/
+# Convert from .biom to .tsv:
+biom convert -i ./q2files/feature-table.biom -o ./q2files/feature-table.tsv --to-tsv
+cat ./q2files/feature-table.tsv | sed 's/ASV//g' | awk '{print "OTU"$0}' | sed 's/OTU#OTU ID/X.OTU.ID/g' > ./otu97table_clean.tsv # rename file, relabel ASVs as OTUs, any perfect matched to database append OTU and move to working directory                                              
+echo;echo "export of sequences and OTU table complete"
+
+# BLAST selection for tree building
+# Replace placeholder text in R script with user-provided truncation lengths for R1 and R2: SplitR1R2
+cat AMFsplitR1R2.R | sed "s/ftrunc/$R1cutoff/" | sed "s/rtrunc/$R2cutoff/" > AMFsplitR1R2withCutoffs.R
+
+echo;echo "Beginning split R1-R2 pipeline..."
+Rscript AMFsplitR1R2withCutoffs.R
+echo;echo “split R1-R2 pipeline complete”
+rm AMFsplitR1R2withCutoffs.R # remove temporary script once it's done running
+
+#Make BLAST reference database
+makeblastdb -in V15_LSUDB_3.23.21_AMFONLY.fasta -input_type fasta -dbtype nucl -out V15_LSUDB_3.23.21_AMFONLY
+
+#Blast R1, extract top hit based on bit score, extract col 1, replace header with feature-id
+blastn -db V15_LSUDB_3.23.21_AMFONLY -query R1.otu97repseqs_clean.fasta -outfmt 6 | sort -k1,1 -k12,12nr -k11,11n | sort -u -k1,1 --merge | cut -f1 > S.BLAST.R1.otu97repseqs_clean.txt 
+
+#Blast R2, extract top hit based on bit score, extract col 1, replace header with feature-id
+blastn -db V15_LSUDB_3.23.21_AMFONLY -query R2.otu97repseqs_clean.fasta -outfmt 6 | sort -k1,1 -k12,12nr -k11,11n | sort -u -k1,1 --merge | cut -f1 > S.BLAST.R2.otu97repseqs_clean.txt
+
+# Join files, get unique OTU and add header
+cat S.BLAST.R1.otu97repseqs_clean.txt S.BLAST.R2.otu97repseqs_clean.txt |sort| uniq | sed -e '1i\otus' > BLAST.R1_R2.otu97repseqs_clean_cut.tsv
+echo; echo "BLAST has been completed on forward and reverse reads; each OTU with a hit saved"
+
+# Subset .fasta and .txt based on this name list:
+Rscript AMFextractBLAST.R
+
+# remove once no longer needed:
+
+rm S.BLAST.R1.otu97repseqs_clean.txt 
+rm S.BLAST.R2.otu97repseqs_clean.txt
+rm BLAST.R1_R2.otu97repseqs_clean_cut.tsv 
+rm R1.otu97repseqs_clean.fasta 
+rm R2.otu97repseqs_clean.fasta 
+
+echo;echo "Sequences and OTU table subset to BLAST positive OTUs"
+
+echo; echo "AMFtrimmedToOTUs.sh has completed running. Be sure to check the output file for error messages."
